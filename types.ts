ad525d5791347a422c9730afaacf6c16932842d9
Jack Smith <jacksmithxyz@protonmail.com>
2025-08-30 11:33:25 +0100

    Add types for anilist response

---
+++ types.ts
@@ -0,0 +1,40 @@
+/* These interfaces don't map exactly to the AniList API,
+ and are built around the returned values in the QUERY object in consts.ts.
+*/
+
+interface StartDate {
+  // We can filter for year in the AniList query so this is always a number
+  year: number;
+  month?: number;
+  day?: number;
+}
+
+interface PageInfo {
+  total: number;
+  currentPage: number;
+  lastPage: number;
+  hasNextPage: boolean;
+}
+
+interface PageData {
+  pageInfo: PageInfo;
+  media: MediaEntry[];
+}
+
+export interface MediaEntry {
+  id: number;
+  title: {
+    english?: string;
+    romaji: string;
+  };
+  startDate: StartDate;
+  description: string;
+  coverImage: string;
+  siteUrl: string;
+}
+
+export interface AnilistResponse {
+  data: {
+    Page: PageData;
+  };
+}
