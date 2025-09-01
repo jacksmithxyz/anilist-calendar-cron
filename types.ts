6bef1df7321c5054c35f7a9ad10a17a8481d6fc5
Jack Smith <jacksmithxyz@protonmail.com>
2025-09-01 15:01:51 +0100

    Add type for anime databse entry format

--- types.ts
+++ types.ts
@@ -2,6 +2,15 @@
  and are built around the returned values in the QUERY object in consts.ts.
 */
 
+export interface DbAnimeEntry {
+  anilistId: number;
+  title: string;
+  releaseDate: Date;
+  description: string;
+  coverImage: string;
+  siteUrl: string;
+}
+
 interface StartDate {
   // We can filter for year in the AniList query so this is always a number
   year: number;
